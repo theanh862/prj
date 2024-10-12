5450b487af0bd361c81bc58aa9ba0d09184cd6b9
theanh862 <theanh812k@gmail.com>
2024-10-12 17:04:12 +0700

    Add age calculator feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Todo from './components/Todo';
-
+import AgeCalculator from './components/AgeCalculator'
 function App() {
   return (
     <div className="App">
       <Todo />
+      <AgeCalculator />
     </div>
   );
 }
